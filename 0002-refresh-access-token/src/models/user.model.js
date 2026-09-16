28913bd0bf047e725d1dcf3acf1d0fcf1783f308
Nauman Mullaji <naumanmullaji3@gmail.com>
2026-09-16 12:54:23 +0530

    added more validation on user schema

--- 0002-refresh-access-token/src/models/user.model.js
+++ 0002-refresh-access-token/src/models/user.model.js
@@ -4,17 +4,23 @@ const userSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
+    minLength: 3,
+    maxLength: 50,
   },
   email: {
     type: String,
     required: true,
     unique: true,
+    match: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+    lowercase: true,
+    trim: true,
   },
-  password: {
+  passwordHash: {
     type: String,
     required: true,
+    minLength: 6,
   },
 });
 
-const userModel = mongoose.model("User", userSchema);
+const userModel = mongoose.model("users", userSchema);
 export default userModel;
